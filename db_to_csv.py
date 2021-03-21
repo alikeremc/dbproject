30f965af80185d0f9ad6de2793082479a462deab
alicevik <cevikali83@yahoo.com>
2021-03-21 14:31:13 +0300

    DB'den CSV Dosyası'na Aktarma eklendi

---
+++ db_to_csv.py
@@ -0,0 +1,52 @@
+import psycopg2
+from psycopg2 import Error
+import datetime
+import csv
+
+dosya_adi='mobil_'+datetime.date.today().strftime('%d%m%Y')+".csv"
+
+csv_file=open(dosya_adi,'w')
+csv_writer=csv.writer(csv_file)
+csv_writer.writerow(['Id','Model','Price'])
+
+try:
+    connection = psycopg2.connect(user='postgres',
+                                  password='postgres',
+                                  host='127.0.0.1',
+                                  port=5432,
+                                  database='postgres'
+                                  )
+
+    cursor = connection.cursor()
+
+    #Boş Liste oluştur.
+    mobil_satir=[]
+
+
+
+    select_sorgu='select*from mobile order by id'
+
+    cursor.execute(select_sorgu)
+
+    data=cursor.fetchall()
+
+    print(type(data))
+    print(data)
+
+    for satir in data:
+        csv_writer.writerow(satir)
+
+
+
+
+
+except (Exception, Error) as e:
+    print('Hata oluştu', e)
+
+finally:
+    csv_file.close()
+
+    if connection:
+        cursor.close()
+        connection.close()
+        print('Bağlantı kapatıldı')
